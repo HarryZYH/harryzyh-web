0ff97d2371d415199091f41ae1fbb2af54a0008e
HarryZYH <harry98zyh@gmail.com>
2021-12-16 20:37:39 +0000

    Playing

---
+++ src/pages/Try.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Try() {
+    return (
+        <div class="bg-white h-screen flex flex-col justify-center items-center">
+            <div class="font-mono text-2xl pb-6 hover:animate-pulse">harryzyh.com</div>
+            <table><tr>
+                <td><img class="w-9 h-9" src="https://img.icons8.com/material-rounded/24/000000/dumbbell.png" /></td>
+                <td><img class="w-8 h-8 ml-4" src="https://img.icons8.com/material-rounded/24/000000/source-code.png" /></td>
+                <td><img class="w-9 h-9 ml-4" src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/64/000000/external-guitar-music-kiranshastry-solid-kiranshastry.png" /></td>
+                <td><img class="w-8 h-8 ml-4" src="https://img.icons8.com/wired/64/000000/hollow-knight.png"/></td>
+                <td><img class="w-8 h-8 ml-4" src="https://img.icons8.com/material-rounded/64/000000/beer.png"/></td>
+            </tr></table>
+        </div>
+    )
+}
