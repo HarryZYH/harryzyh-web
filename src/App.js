368c59f91dec398940d569b8e437c7544b77f810
HarryZYH <harry98zyh@gmail.com>
2022-01-04 19:02:11 +0000

    nav bar init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import Try from './pages/Try.js';
-import Home from './pages/Home.js'
 
 function App() {
   return (
-    <Try />
+    <nav class="bg-black">
+      <div class="max-w-9xl h-24 mx-auto border border-red-400">
+        <div class="flex justify-between pt-6">
+          
+          <div class="pl-6">
+            <a href="https:www.youtube.com">
+              <img src="https://img.icons8.com/material-outlined/48/ffffff/death-insurance.png" alt="home"/>
+            </a>
+          </div>
+          
+          <div class="flex justify-between">
+            <div class="pr-6">
+              <a href="https://github.com/">
+                <img src="https://img.icons8.com/material-outlined/48/ffffff/github.png" alt="github"/>
+              </a>
+            </div>
+
+            <div class="pr-4">
+              <a href="https://www.w3schools.com">
+                <img src="https://img.icons8.com/material-outlined/48/ffffff/instagram-new--v1.png" alt="ins"/>
+              </a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </nav>
   );
 }
 
